f86c0843272647cb86a9c9a312a3724c1810c572
antden99 <antoniodenigris@icloud.com>
2024-05-20 16:05:49 +0200

    I add the modification of the name of the routes and of the provides

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController; //controlla sempre che il controller sia importato correttamente
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +20,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+//Adesso devo andare a definire le middleware, che risponderanno tutte allo stesso nome iniziale, e che saranno accessibili solo se si è autenticati e validati
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')    //questo mi permette di concatenare questo nome ad altri nomi delle rotte che verranno definite successivamente in group, ricordati del .
+    ->prefix('admin')   //questo mi permette di utilizzare questo prefisso per le mie nuove rotte
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard'); //qui praticamente sto dicendo che quando c'è /dashboard nell'url il controller gestisce il metodo index e ritorna una pagina, ho tolto '/dashboard' e messo solo '/', perchè in questo modo quando sarò autenticato, con solo admin trovero questa rotta
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+
+//dopo aver modificato il nome delle rotte a riga 26 e riga 29, ricordati di andare nella cartella Providers in RouteService e modificare la pagina di atterraggio una volta loggato correttamente
